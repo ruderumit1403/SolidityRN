3b3189f168b450f292dc071230c08c8d788e4c82
rumit nagori <83534966+ruderumit1403@users.noreply.github.com>
2023-05-03 23:34:13 +0530

    Update Creation of array using while loop.sol

--- Creation of array using while loop.sol
+++ Creation of array using while loop.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-//contract array{
-//    uint [5] public a=[12,23,44,74,77];
-//}
+
 
 contract array{
-    uint [10] public a;    //array of size 10
-    uint public c;         //state variable to add +1 in the array 
-    function loop() public
+    uint [10] public a;   //This declares a public array a of length 10, where each element is of type uint.
+    uint public c;         // This declares a public integer variable c.
+    function loop() public //This declares a public function called loop() that has no parameters and returns no value.
     {
-        while(c<a.length){                //using while loop until the c is less than the length of the array
-            a[c] = c;                    // adding values to the array   
-            c++;                         //returns the value c
+        while(c<a.length){                //This sets up a while loop that will continue iterating over the array a as long as the index variable c is less than the length of the array a.
+            a[c] = c+33;                    // This assigns the value of c + 3 to the element of the array a at index c. 
+            c++;                         //This increments the value of the index variable c by 1 after each iteration of the while loop.
         }
     }
 }
